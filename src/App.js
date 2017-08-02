26f7c64a7982158e328ca99c758dbfa716fdb81a
Mojganx <mojgan.zolghadr@gmail.com>
2017-08-02 11:26:05 +0200

    added sign-up, sign-in and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { PureComponent } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import PropTypes from 'prop-types'
 import Lobby from './game/Lobby'
 import Title from './component/Title.js'
+import Navigation from './component/Navigation'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import muiTheme from './assets/styles/themes'
 import './App.css';
 
 class App extends PureComponent {
+  static childContextTypes = {
+    muiTheme: PropTypes.object.isRequired,
+  }
+
+  getChildContext(){
+    // return  { muiTheme }
+  }
 
   updateGame(id, update) {
     // We will clean this up later
@@ -12,15 +23,19 @@ class App extends PureComponent {
 
   render() {
     return (
+      <MuiThemeProvider muiTheme={muiTheme}>
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+            <Navigation />
+
           <Title content="Lobby" />
         </div>
         <p className="App-intro">
           <Lobby updateGame={ this.updateGame.bind(this) } />
         </p>
+        {this.props.children}
       </div>
+      </MuiThemeProvider>
     );
   }
 }
